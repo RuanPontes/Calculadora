84cdf22a6ec2947e46a6e0e702363c5a3b85e8b1
RuanPontes <ruanpontesfrazao@gmail.com>
2022-04-09 08:58:48 -0300

    teste

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,15 +5,43 @@ public class Calculadora {
 
         int conta1;
         int conta2;
+        double [] notas = new double[4];
 
 
         Scanner insira = new Scanner(System.in);
 
-        System.out.println("\nBem vindo a calculadora em Java.");
+        System.out.println("\nBem vindo a calculadora em Java.\n");
+        System.out.println("-----------------------------------------------------\n");
 
 
-        System.out.println("Deseja fazer uma adição,subtração ou multiplicação?");
+        System.out.println("Deseja fazer qual operação? \nadição.\nsubtração.\nmultiplicação.\nmédia de notas.\nraiz quadrada.");
         String conta = insira.next();
+
+
+        if (conta.equalsIgnoreCase("raiz")) {
+            System.out.println("Digite o número para saber a raiz quadrada: ");
+            conta1 = insira.nextInt();
+
+            double raiz = (Math.sqrt(conta1));
+
+            System.out.println("A raiz do número " + conta1 + " é igual a " + raiz);
+        }
+
+        if (conta.equalsIgnoreCase("Média")){
+            for (int i = 0; i < notas.length ; i++) {
+                System.out.println("Digite a nota "+ (i + 1));
+                notas[i]= insira.nextDouble();
+
+            }
+            double media = 0;
+
+            for (int i = 0; i < notas.length ; i++) {
+                media = media + notas [i];
+                System.out.println("Nota " + (i + 1) + ": "+notas[i]);
+            }
+            media = media / notas.length;
+            System.out.println("Media das notas: "+media);
+        }
 
         if (conta.equalsIgnoreCase("adição")){
             System.out.println("\nDigite o primeiro número para adição ");
@@ -53,8 +81,10 @@ public class Calculadora {
         }
 
 
-        while (!conta.equalsIgnoreCase("adição") & !conta.equalsIgnoreCase("subtração") & !conta.equalsIgnoreCase("divisão") & !conta.equalsIgnoreCase("multiplicação") ){
-            System.out.println("Insira novamente qual operação você deseja fazer:");
+        while (!conta.equalsIgnoreCase("adição") & !conta.equalsIgnoreCase("subtração") & !conta.equalsIgnoreCase("divisão") & !conta.equalsIgnoreCase("multiplicação") &!conta.equalsIgnoreCase("Média") &!conta.equalsIgnoreCase("raiz") ){
+            System.out.println("\nInsira novamente qual operação você deseja fazer.");
+            System.out.println("Lembre-se de usar palavras chaves como: adição,subtração,divisão,raiz,média:");
+            System.out.println("-----------------------------------------------------");
             conta = insira.next();
 
 
@@ -94,10 +124,35 @@ public class Calculadora {
                 System.out.println("O resultado foi = " + (conta1 / conta2));
             }
 
+            if (conta.equalsIgnoreCase("Média")){
+                for (int i = 0; i < notas.length ; i++) {
+                    System.out.println("Digite a nota "+ (i + 1));
+                    notas[i]= insira.nextDouble();
+
+                }
+                double media = 0;
+
+                for (int i = 0; i < notas.length ; i++) {
+                    media = media + notas [i];
+                    System.out.println("Nota " + (i + 1) + ": "+notas[i]);
+                }
+                media = media / notas.length;
+                System.out.println("Media das notas: "+media);
+            }
+
+            if (conta.equalsIgnoreCase("raiz")) {
+                System.out.println("Digite o número para saber a raiz quadrada: ");
+                conta1 = insira.nextInt();
+
+                double raiz = (Math.sqrt(conta1));
+
+                System.out.println("A raiz do número " + conta1 + " é igual a " + raiz);
+            }
+
 
         }
-
-        System.out.println("\nO programa foi executado com sucesso! ");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("O programa foi executado com sucesso! ");
 
 
     }}
